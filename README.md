69cfe2b91065b8b06f5e7cb14a0f7c1c19ebe5dc
jiriletak <jiri.letak@ibillboard.com>
2020-07-30 23:08:25 +0200

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Markdown to string action
+
+This action transform markdown to string
+
+## Inputs
+
+### `file-path`
+
+**Required** path to `.md` file
+
+## Outputs
+
+### `result`
+
+formatted string 
+
+## Example usage
+
+uses: actions/markdown-to-string@v1
+with:
+  file-path: './README.md'
---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const core = require('@actions/core')
+const { promises: fs } = require('fs')
+
+const main = async () => {
+    const path = core.getInput('file-path')
+    const content = await fs.readFile(path, 'utf8')
+
+    const formattedContent = content.replace('\r', '\\r')
+        .replace('\n', '\\n')
+        .replace('"','\"');
+
+    core.setOutput("result", formattedContent);
+}
+
+main().catch(err => core.setFailed(err.message))
